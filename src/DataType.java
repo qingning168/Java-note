5d638b91aa27a3af901dfb442715a73191b16a0f
清柠 <48634712+qingning168@users.noreply.github.com>
2019-07-20 22:06:41 +0800

    Create DataType.java

---
+++ src/DataType.java
@@ -0,0 +1,10 @@
+public class DataType{
+    public static void main(String[] args){
+        short s = 1000;
+        int i = 10000;
+        long a = 10000L;
+        float f = 123.4f;
+        double b = 123.4;
+        system.out.println(s);
+    }
+}
